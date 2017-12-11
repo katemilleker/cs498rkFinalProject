cef24e7088b713a1e92e836820924a90582e7c25
Rohan Subramaniam <rcsubra2@illinois.edu>
2017-12-10 19:44:18 -0600

    fixed for android

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,25 +1,27 @@
 
 import axios from "axios";
-import querystring from "querystring";
+//import querystring from "querystring";
+
+var host = "localhost";
 
 module.exports.logInJobSeeker = (user) => {
-  let url = "http://localhost:3000/login";
-  return axios.post(url, querystring.stringify(user));
+  let url = "http://10.0.2.2:3000/login";
+  return axios.post(url, user);
 };
 
 module.exports.logInRecruiter = (user) => {
-  let url = "http://localhost:3000/loginrec";
-  return axios.post(url, querystring.stringify(user));
+  let url = "http://${host}:3000/loginrec";
+  return axios.post(url, user);
 };
 
 module.exports.signUpJobSeeker = (user) => {
   console.log("IN SIGN UP JOB SEEKER");
-  let url = "http://localhost:3000/register";
-  return axios.post(url, querystring.stringify(user));
+  let url = "http://${host}:3000/register";
+  return axios.post(url, user);
 };
 
 module.exports.signUpRecruiter = (user) => {
   console.log("IN SIGN UP RECRUITER");
-  let url = "http://localhost:3000/registerrec";
-  return axios.post(url, querystring.stringify(user));
+  let url = "http://${host}:3000/registerrec";
+  return axios.post(url, user);
 };
