534c34d4a4e31f369951661c1146bf9f3c677f7d
Daniel Jamrozik <djamro2@illinois.edu>
2017-12-10 19:47:14 -0600

    auth interpolation

--- app/api/auth.js
+++ app/api/auth.js
@@ -5,23 +5,21 @@ import axios from "axios";
 var host = "localhost";
 
 module.exports.logInJobSeeker = (user) => {
-  let url = "http://10.0.2.2:3000/login";
+  let url = `http://${host}:3000/login`;
   return axios.post(url, user);
 };
 
 module.exports.logInRecruiter = (user) => {
-  let url = "http://${host}:3000/loginrec";
+  let url = `http://${host}:3000/loginrec`;
   return axios.post(url, user);
 };
 
 module.exports.signUpJobSeeker = (user) => {
-  console.log("IN SIGN UP JOB SEEKER");
-  let url = "http://${host}:3000/register";
+  let url = `http://${host}:3000/register`;
   return axios.post(url, user);
 };
 
 module.exports.signUpRecruiter = (user) => {
-  console.log("IN SIGN UP RECRUITER");
-  let url = "http://${host}:3000/registerrec";
+  let url = `http://${host}:3000/registerrec`;
   return axios.post(url, user);
 };
