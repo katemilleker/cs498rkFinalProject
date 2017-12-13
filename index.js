b91d074c344dcb7e0120db73458ef4439cbdfb27
Daniel Jamrozik <djamro2@illinois.edu>
2017-12-12 18:50:21 -0600

    enable expo for this project

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './app/index.js';
 
+// register root app component
 AppRegistry.registerComponent('resume_recruiter', () => App);
+
+// this line is required for expo
+AppRegistry.registerComponent('main', () => App);
